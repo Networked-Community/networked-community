49d7482c29a5209a3db281c008a86ff6135e4bd3
beriniwlew <berin@networked.community>
2020-04-20 10:43:56 -0400

    update: Updated to Orchard v1.0.0-rc1-12867
    Bound References to Feed

--- src/core/Nc.Cms.Web/Startup.cs
+++ src/core/Nc.Cms.Web/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -35,6 +36,8 @@ namespace Nc.Cms.Web
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
